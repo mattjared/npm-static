36521642bd4455e89e6b2b422b61cb1726615c45
mattjared <mattjared9@gmail.com>
2016-02-21 21:19:49 -0600

    structure tweaks, jade partials, watch command

---
+++ dist/script.js
@@ -0,0 +1,1 @@
+var thing="hello";if(thing.length>7){console.log(thing)}else{console.log("goodbye")}
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+var thing = "hello";
+if (thing.length > 7) {
+    console.log(thing);
+}
+else {
+    console.log("goodbye");
+}
